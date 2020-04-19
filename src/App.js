0766ece441bb4b4b7e19ff2aa0e9a4f00dc976da
james <lebronjames_lakers@163.com>
2020-04-19 22:27:46 +0800

    second

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button,message} from 'antd'
+//自定义一个App组件
+export default class App extends React.Component{
+    //此时这里用箭头函数。会保证下面this的准确性
+    handleClick=()=>{
+        message.success("成功啦，我的React项目搭起来啦")
+    }
+    render(){
+        return <Button type="primary" onClick={this.handleClick}>点击我</Button>
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
-
-export default App;
