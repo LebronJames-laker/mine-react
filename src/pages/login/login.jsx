26308670f75e8a034e99677b4e9b8e73e755ca8b
LebronJames-laker <lebronjames_lakers@163.com>
2020-04-20 16:28:17 +0800

    loginPage

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,37 +2,18 @@ import React from 'react';
 import './login.less';
 import logo from './images/logo.jpg';
 import { Form, Input, Button, Checkbox,message } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-
-    const layout = {
-        labelCol: {
-        span: 8,
-        },
-        wrapperCol: {
-        span: 16,
-        },
-    };
-    const tailLayout = {
-        wrapperCol: {
-        offset: 8,
-        span: 16,
-        },
-    };
     
-
-        const onFinish = values => {
-        console.log('Success:', values);
-        };
-    
-        const onFinishFailed = errorInfo => {
-        console.log('Failed:', errorInfo);
-        };
    
 //自定义一个App组件
 export default class Login extends React.Component{
-    //此时这里用箭头函数。会保证下面this的准确性
+    
+    
     
     render(){
+        // const form = this.props.form
+        // const {getFieldDecorator} = form
         return(
             <div className='login'>
                 <header className='login-header'>
@@ -41,53 +22,54 @@ export default class Login extends React.Component{
                 </header>
                 <section className='login-content'>
                     <h1>用户登录</h1>
-                    <Form className='myForm'
-                        {...layout}
-                        name="basic"
-                        initialValues={{
-                            remember: true,
-                        }}
-                        onFinish={onFinish}
-                        onFinishFailed={onFinishFailed}
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{ remember: true }}
+                    
                         >
                         <Form.Item
-                            label="用户名"
                             name="username"
-                            rules={[
-                            {
-                                required: true,
-                                message: 'Please input your username!',
-                            },
-                            ]}
+                            rules={[{ required: true, message: 'Please input your Username!' }]}
                         >
-                            <Input />
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
                         </Form.Item>
-
                         <Form.Item
-                            label="密码"
                             name="password"
-                            rules={[
-                            {
-                                required: true,
-                                message: 'Please input your password!',
-                            },
-                            ]}
+                            rules={[{ required: true, message: 'Please input your Password!' }]}
                         >
-                            <Input.Password />
+                            <Input
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                            />
                         </Form.Item>
-
-                        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                        <Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
                             <Checkbox>记住我</Checkbox>
+                            </Form.Item>
+
                         </Form.Item>
 
-                        <Form.Item {...tailLayout}>
-                            <Button type="primary" htmlType="submit" id='myButton'>
+                        <Form.Item>
+                            <Button id='loginButton' type="primary" htmlType="submit" className="login-form-button">
                             登录
                             </Button>
+                        
                         </Form.Item>
                     </Form>
                 </section>
             </div>
         );
     }
+
+
+
+
+
 }
+
+// //利用一个高阶函数，传入Login这个包含form表单的组件，得到一个新组件，利用新组件传递给Login组件一个form属性
+// const NewComponent = Form.create()(Login)
+// //暴露的是这个新的组件
+// export default NewComponent
